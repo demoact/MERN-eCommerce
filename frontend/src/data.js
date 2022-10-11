2f58ad92ecd8141d22c996e649386595a01b1030
Cart <carthqviva2@gmail.com>
2022-10-12 00:34:13 +0530

    video-06-product-listing

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Nike slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirts',
+    },
+    {
+      name: 'Nike slim shirt 2',
+      slug: 'nike-slim-shirt-2',
+      category: 'shirts',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirts',
+    },
+    {
+      name: 'Nike slim shirt 3',
+      slug: 'nike-slim-shirt-3',
+      category: 'shirts',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirts',
+    },
+  ],
+};
+
+export default data;
