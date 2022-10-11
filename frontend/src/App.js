7efcba2b85e1f56267eef59a8fbdc1b170bd5a86
Cart <carthqviva2@gmail.com>
2022-10-11 23:37:07 +0530

    video04-git-control

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
