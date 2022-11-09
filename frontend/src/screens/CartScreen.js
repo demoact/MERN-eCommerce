16de727e850091847c2981e59894e73f15e3dedc
Cart <carthqviva2@gmail.com>
2022-11-09 08:38:21 +0530

    Video 13 - improved -add cart

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,39 @@
+import { useContext } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Store } from '../Store';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function CartScreen() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  return (
+    <div>
+      <Helmet>
+        <title>Shopping Cart</title>
+      </Helmet>
+      <h1>Shopping Cart</h1>
+      <Row>
+        <Col md={8}>
+          {cartItems.length === 0 ? (
+            <MessageBox>
+              Cart is empty. <link to="/">Go Shopping</link>
+            </MessageBox>
+          ) : (
+            <ListGroup>
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item._id}>
+                  <Row className="align-items-center"></Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+    </div>
+  );
+}
