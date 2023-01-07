4091af7a5425da895111289f2b922cdbed677d3a
Cart <carthqviva2@gmail.com>
2023-01-07 08:41:29 +0530

    prx change

--- backend/server.js
+++ backend/server.js
@@ -41,5 +41,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Serve at the port http://localhost:${port}`);
+  console.log(`Serve at the ports http://localhost:${port}`);
 });
