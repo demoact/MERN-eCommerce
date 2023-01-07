b66ad9ae451402c32b1fb04f82364eb43043163d
Cart <carthqviva2@gmail.com>
2023-01-07 09:04:16 +0530

    fgf

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,14 @@ mongoose
   });
 
 const app = express();
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+  Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
