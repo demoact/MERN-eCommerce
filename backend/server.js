bdd0cab9e3a263282bcb0914b038863adfeafd60
Cart <carthqviva2@gmail.com>
2023-01-11 08:19:37 +0530

    add build path in server.js

--- backend/server.js
+++ backend/server.js
@@ -43,3 +43,16 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at the port http://localhost:${port}`);
 });
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+// Handle GET requests to /api route
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
